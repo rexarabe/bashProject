b08c8aaf7254efeea555b7c9d97946ccab610de3
rexarabe <ab28041993@gmail.com>
2020-01-04 18:54:33 +0200

    Creation of first.sh, which I will modifie later to write what I have notice on the file

---
+++ first.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#This is a comment
+
+echo "Hello   World" #The output will hello world
+echo "Hello Wold"
+echo "Hello * World"
+echo Hello * World
+echo Hello    World
+echo "Hello" World
+echo Hellp "   " World
+echo "Hello "*" World"
+echo `hello` world
+echo 'hello' world
