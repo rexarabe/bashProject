4571462cbe1eb6decc2ee0e166fbecaefde66f6b
rexarabe <ab28041993@gmail.com>
2020-01-07 17:06:36 +0200

    I did add some comments, that descibe the outcome of each line

--- first.sh
+++ first.sh
@@ -3,12 +3,12 @@
 #This is a comment
 
 echo "Hello   World" #The output will hello world
-echo "Hello Wold"
-echo "Hello * World"
-echo Hello * World
-echo Hello    World
-echo "Hello" World
-echo Hellp "   " World
-echo "Hello "*" World"
-echo `hello` world
-echo 'hello' world
+echo "Hello World"    #Hello World
+echo "Hello * World"  #Hello * World
+echo Hello * World    #Hello README.md first.sh my-script.sh World
+echo Hello    World   #Hello World
+echo "Hello" World    #Hello World
+echo Hellp "   " World  #Hellp     World
+echo "Hello "*" World"     #Hello * World
+echo `hello` world        #world
+echo 'hello' world        #hello world
